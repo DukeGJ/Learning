e864dd8b83f5f3fc4c77322206eae3c87b39bdf8
Michael <michael.guojing@gmail.com>
2018-12-22 23:43:17 +0800

    Thread Demo

---
+++ bounceThread/BouceThread.java
@@ -0,0 +1,85 @@
+package bounceThread;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import bounce.Ball;
+import bounce.BallComponent;
+
+public class BouceThread {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new BounceFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		});
+	}
+
+}
+
+class BounceFrame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BallComponent comp;
+	public static final int STEPS = 1000;
+	public static final int DELAY = 3;
+
+	/**
+	 * Constructs the frame with the component for showing the bouncing ball and
+	 * Start and Close buttons;
+	 */
+	public BounceFrame() {
+		setTitle("Bounce");
+		comp = new BallComponent();
+		add(comp, BorderLayout.CENTER);
+		JPanel buttonPanel = new JPanel();
+		addButton(buttonPanel, "Start", event -> addBall());
+		addButton(buttonPanel, "Close", event -> System.exit(0));
+		add(buttonPanel, BorderLayout.SOUTH);
+		pack();
+	}
+
+	/**
+	 * Add a button to a container.
+	 * 
+	 * @param c
+	 *            the container
+	 * @param title
+	 *            the button title
+	 * @param listner
+	 *            the action listener for the button
+	 */
+	public void addButton(Container c, String title, ActionListener listner) {
+		JButton button = new JButton(title);
+		c.add(button);
+		button.addActionListener(listner);
+	}
+
+	public void addBall() {
+		Ball ball = new Ball();
+		comp.addBall(ball);
+		Runnable r = () -> {
+			for (int i = 1; i <= STEPS; i++) {
+				ball.move(comp.getBounds());
+				comp.paint(comp.getGraphics());
+				try {
+					Thread.sleep(DELAY);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			System.err.println("多线程的方式添加Ball");
+		};
+		Thread t = new Thread(r);
+		t.start();
+	}
+}
