b574de3a1a6571abe1cb9a3e709ef354683a21df
michael.guojing <1298684178@qq.com>
2019-03-26 18:52:14 +0800

    Hibernate配置文件,ORM配置文件,ORM实体类

---
+++ HibernateLearning/src/entry/User.java
@@ -0,0 +1,72 @@
+package entry;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+public class User {
+    private int id;
+    private String name;
+    private Timestamp birthday;
+    private Double money;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "birthday")
+    public Timestamp getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Timestamp birthday) {
+        this.birthday = birthday;
+    }
+
+    @Basic
+    @Column(name = "money")
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday) &&
+                Objects.equals(money, user.money);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, birthday, money);
+    }
+}
